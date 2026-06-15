56cdaa0d5fdbad09f12a0befaee8e5467140e17f
YoYoMastr <petertheyoyomaster@gmail.com>
2026-06-15 15:26:11 -0500

    Xp Hotkey Change pt2

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,7 +5,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "infinite xp";
 
-    public const string Description = "Go in a regular singleplayer game, then press 0 now works on Mermonkey, Desperado and Beast Handler! (Also gives 2147483647 xp instead of 1000000) (All credit to Timotheeee for the Orginal mod I just updated it to work on the newer towers and give more xp)";
+    public const string Description = "Go in a regular singleplayer game, then press ctrl+0 for tower xp and ctrl+9 for player xp! This works on Mermonkey, Desperado and Beast Handler! (Also gives 2147483647 xp instead of 1000000) (All credit to Timotheeee for the Original mod I just updated it to work on the newer towers and give more xp)";
  public const string SubPath = "infinite_xp";
  public const string DllName = "infinite_xp.dll";
  public const string WorksOnVersion = "55";
